f9b6338b4c570d49723aceccbeddb72dfa5e94c6
howardrlee <howardrlee@yahoo.com>
2022-08-12 10:46:18 -0400

    Finished Gatsby Tutorial Part 2

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -1,6 +1,8 @@
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=default" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=default" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=default" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-gallery-js": () => import("./../../../src/pages/gallery.js?export=default" /* webpackChunkName: "component---src-pages-gallery-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=default" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
@@ -9,6 +11,8 @@ exports.components = {
 exports.head = {
   "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js?export=head" /* webpackChunkName: "component---cache-dev-404-page-jshead" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js?export=head" /* webpackChunkName: "component---src-pages-404-jshead" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js?export=head" /* webpackChunkName: "component---src-pages-about-jshead" */),
+  "component---src-pages-gallery-js": () => import("./../../../src/pages/gallery.js?export=head" /* webpackChunkName: "component---src-pages-gallery-jshead" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js?export=head" /* webpackChunkName: "component---src-pages-index-jshead" */)
 }
 
--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -6,6 +6,8 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("C:\\dev\\gatsby\\Desktop\\my-first-gatsby-site\\.cache\\dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("C:\\dev\\gatsby\\Desktop\\my-first-gatsby-site\\src\\pages\\404.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\dev\\gatsby\\Desktop\\my-first-gatsby-site\\src\\pages\\about.js")),
+  "component---src-pages-gallery-js": preferDefault(require("C:\\dev\\gatsby\\Desktop\\my-first-gatsby-site\\src\\pages\\gallery.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\dev\\gatsby\\Desktop\\my-first-gatsby-site\\src\\pages\\index.js"))
 }
 
